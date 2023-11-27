11a9807970512991e796b9914bc276e3084aa53b
Jerlin shabi <138958887+jerlin-shabi@users.noreply.github.com>
2023-11-27 06:19:57 +0000

    sucessfully impoted from copilot

---
+++ SelectionSort.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Array before sorting:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        selectionSort(arr);
+
+        System.out.println("\nArray after sorting:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
